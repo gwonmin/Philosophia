e35676b343bf1e5960b08e4c8ef21a974b3cbc6c
jjs0211 <jjs_0211_@naver.com>
2022-06-07 13:33:52 +0900

    [Add]: cors 설치

--- back/app.js
+++ back/app.js
@@ -1,3 +1,5 @@
+import cors from "cors";
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -8,6 +10,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
