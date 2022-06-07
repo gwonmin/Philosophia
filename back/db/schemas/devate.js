ab8fdfdde35ed688f6d074ea42a16ce95ab6f31d
yuhayan <cnessit@gmail.com>
2022-06-07 11:51:44 +0900

    [Add]: devate 스키마 초안

---
+++ back/db/schemas/devate.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const DevateSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: '', // 유저 스키마 참조
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        tag: {
+            type: Array,
+            required: false,
+            default: [],
+        },
+        yes: [{ type: String }],
+        yesCount: {
+            type: Number,
+            default: 0,
+        },
+        no: [{ type: String }],
+        noCount: {
+            type: Number,
+            default: 0,
+        },
+        comment: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: '' // 댓글 스키마 참조
+            }
+        ],
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const devateModel = model('Devate', DevateSchema);
+
+module.exports = devateModel;
