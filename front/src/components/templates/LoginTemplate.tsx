64b45bfb42087341662f409a3facafd4e095eb64
Hyun <watalu1209@gmail.com>
2022-06-12 18:08:30 +0900

    [Update]: 임시 Header 분리

--- front/src/components/templates/LoginTemplate.tsx
+++ front/src/components/templates/LoginTemplate.tsx
@@ -1,7 +1,7 @@
-import { useNavigate } from "react-router-dom"
 import Container from "@mui/material/Container"
 
 import LoginForm from "../organisms/LoginForm"
+import Header from "../organisms/Header"
 
 type User = {
   email: string
@@ -9,28 +9,12 @@ type User = {
   name: string
 }
 export default function LoginTemplate({ login, userInfo }: { login?: boolean; userInfo?: User }) {
-  const navigate = useNavigate()
-
   const initUser: User = {
     email: "",
     password: "",
     name: "",
   }
 
-  const Header = () => {
-    return (
-      <div>
-        <p>헤더가 올 예정입니다.</p>
-        <button
-          onClick={() => {
-            navigate("/", { replace: true })
-          }}
-        >
-          감시 페이지로 가기
-        </button>
-      </div>
-    )
-  }
   const Footer = () => {
     return <p>푸터도 오게 될까요?</p>
   }
--- front/src/components/templates/RegisterTemplate.tsx
+++ front/src/components/templates/RegisterTemplate.tsx
@@ -1,7 +1,7 @@
-import { useNavigate } from "react-router-dom"
 import Container from "@mui/material/Container"
 
 import RegisterForm from "../organisms/RegisterForm"
+import Header from "../organisms/Header"
 
 type User = {
   email: string
@@ -9,28 +9,12 @@ type User = {
   name: string
 }
 export default function RegisterTemplate({ register, userInfo }: { register?: boolean; userInfo?: User }) {
-  const navigate = useNavigate()
-
   const initUser: User = {
     email: "",
     password: "",
     name: "",
   }
 
-  const Header = () => {
-    return (
-      <div>
-        <p>헤더가 올 예정입니다.</p>
-        <button
-          onClick={() => {
-            navigate("/", { replace: true })
-          }}
-        >
-          감시 페이지로 가기
-        </button>
-      </div>
-    )
-  }
   const Footer = () => {
     return <p>푸터도 오게 될까요?</p>
   }
