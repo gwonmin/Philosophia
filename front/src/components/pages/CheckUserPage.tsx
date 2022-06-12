7b7ae6a3d3b14060f8e89901693a1b9899f5e408
jjs0211 <jjs_0211_@naver.com>
2022-06-12 23:26:17 +0900

    [Fix]: front 파일 삭제

--- front/src/components/pages/CheckUserPage.tsx
+++
@@ -1,5 +0,0 @@
-import LoginTemplate from "../templates/LoginTemplate";
-
-export default function LoginPage({ user }: any) {
-  return <LoginTemplate login={false} userInfo={user} />;
-}
--- front/src/components/pages/EditUserInfoPage.tsx
+++
@@ -1,5 +0,0 @@
-import RegisterTemplate from "../templates/RegisterTemplate";
-
-export default function RegisterPage({ user }: any) {
-  return <RegisterTemplate register={false} userInfo={user} />;
-}
