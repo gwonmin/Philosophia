f646495cd0d3e0242b9bfb0a8c768a648885c6f0
hajun <audgkwns44@gmail.com>
2022-06-12 17:00:33 +0900

    Create atom for profile image

--- front/src/components/atoms/profile.tsx
+++ front/src/components/atoms/profile.tsx
@@ -1,9 +1,10 @@
 import { ImageProps } from "../models/ProfileProps";
 
-export function ProfileImage({src, width, height}: ImageProps){
+export function ProfileImage({src}: ImageProps){
+    const defaultImageUrl = "../../../public/defaultImage.png"
     return (
         <img 
-            src={src}
+            src={src || defaultImageUrl}
             style={{
                 width: "90px",
                 height: "60px",
