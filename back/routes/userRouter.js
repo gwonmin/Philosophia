4a454bb5bd33b69b6a05e60ce20871f79e938286
jjs0211 <jjs_0211_@naver.com>
2022-06-14 13:38:20 +0900

    [Fix]: 회원정보 변경 API 오류 해결 및 수정

--- back/routes/userRouter.js
+++ back/routes/userRouter.js
@@ -79,7 +79,7 @@ userRouter.put("/user/:userId", verifyToken, async function (req, res, next) {
     const email = req.body.email ?? null;
     const password = req.body.password ?? null;
     const name = req.body.name ?? null;
-    const toUpdate = { name, email, password, description, visited };
+    const toUpdate = { name, email, password };
 
     const updatedUser = await userService.setUser({ userId, toUpdate });
 
