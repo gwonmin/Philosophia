4f78caf77228854df277080767256b989d978f83
jjs0211 <jjs_0211_@naver.com>
2022-06-14 12:13:13 +0900

    [Add]: 철학자별 게시판 니체 router 구현

---
+++ back/routes/philosopherRouter.js
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import { philosopherService } from "../services/philosopherService";
+import { verifyToken } from "../middlewares/verifyToken";
+import { verifyRefresh } from "../middlewares/verifyRefresh";
+
+const philosopherRouter = Router();
+
+//니체 게시판 게시글 작성
+philosopherRouter.post("/nietzsche", verifyToken, async function(req, res, next){
+    try{
+        const userId = req.user;
+        const { title, content } = req.body;
+
+        if (!title || !content){
+            throw new Error("제목과 내용을 입력해주세요.");
+        };
+
+        const newPost = await philosopherService.addPost({
+            userId,
+            title,
+            content,
+        });
+
+        if (newPost.errorMessage) {
+            throw new Error(newPost.errorMessage);
+          };
+          res.status(201).json(newPost);        
+        } catch (error) {
+            next(error)
+        };
+});
+
+// 니체 게시판 게시글 상세 조회
+philosopherRouter.get("/nietzsche/:id", verifyToken, async function(req, res, next){
+    try{
+        const postId = req.params.id;
+        const currentPostInfo  = await philosopherService.getPostInfo({ postId });
+
+        if (currentPostInfo.errorMessage) {
+            throw new Error(currentPostInfo.errorMessage);
+        }
+        res.status(200).send(currentPostInfo);
+
+    } catch (error){
+        next(error);
+    };
+});
+
+// 니체 게시판 게시글 전체 조회
+philosopherRouter.get("/nietzsche", verifyToken, async function(req, res, next){
+    try{
+        const posts = await philosopherService.getPostList();
+        res.status(200).send(posts);
+    } catch (error){
+        next(error);
+    };
+});
+
+// 니체 게시판 게시글 수정
+philosopherRouter.put("/nietzsche/:id", verifyToken, async function(req, res, next){
+    try{
+        const postId = req.params.id;
+
+        const title = req.body.title ?? null;
+        const content = req.body.content ?? null;
+
+        const toUpdate = { title, content };
+
+        const updatedPost = await philosopherService.setPost({ postId, toUpdate });
+
+        if (updatedPost.errorMessage) {
+            throw new Error(updatedPost.errorMessage);
+        }
+        res.status(200).json(updatedPost);
+    } catch (error){
+        next(error);
+    };
+});
+
+// 니체 게시판 게시글 삭제
+philosopherRouter.delete("/nietzsche/:id", verifyToken, async function(req, res, next){
+    try{
+        const postId = req.params.id;
+
+        const deletedPost = await philosopherService.deletePost({ postId });
+        if (deletedPost.errorMessage) {
+            throw new Error(deletedPost.errorMessage);
+        }
+
+        res.status(200).send(deletedPost)
+    } catch (error){
+        next(error);
+    };
+});
