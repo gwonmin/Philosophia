675f88b1d3db8e1df2e2a7db6a473b8e40da6b96
yuhayan <cnessit@gmail.com>
2022-06-17 17:18:40 +0900

    [Fix]: 찬반 기능 수정 완료

--- back/db/schemas/devate.js
+++ back/db/schemas/devate.js
@@ -1,49 +1,42 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 const DevateSchema = new Schema(
-    {
-        author: {
-            type: Schema.Types.ObjectId,
-            ref: 'User', // 유저 스키마 참조
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        content: {
-            type: String,
-            required: true
-        },
-        tag: {
-            type: Array,
-            required: false,
-            default: [],
-        },
-        yes: [{ type: String }],
-        yesCount: {
-            type: Number,
-            default: 0,
-        },
-        no: [{ type: String }],
-        noCount: {
-            type: Number,
-            default: 0,
-        },
-        comment: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'DevateComment', // 댓글 스키마 참조
-            }
-        ],
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // 유저 스키마 참조
+      required: true,
     },
-    {
-        timestamps: true,
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey: false 
-    }
-)
+    content: {
+      type: String,
+      required: true,
+    },
+    tag: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    yes: [{ type: String }],
+    no: [{ type: String }],
+
+    comment: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'DevateComment', // 댓글 스키마 참조
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const DevateModel = model('Devate', DevateSchema);
 
