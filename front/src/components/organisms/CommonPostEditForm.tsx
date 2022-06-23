c93062c9efc180f785782b2b5a412247e8d86447
Hyun <watalu1209@gmail.com>
2022-06-23 22:26:15 +0900

    [Update]: C-P-EditForm을 업데이트했습니다.

--- front/src/components/organisms/CommonPostEditForm.tsx
+++ front/src/components/organisms/CommonPostEditForm.tsx
@@ -3,64 +3,66 @@ import { Container } from "@mui/material"
 import { TextFieldAtom } from "../atoms/textInputs"
 import { handleChange, handleEdit } from "../../util"
 
-//-------------------------------------------Devate-------------------------------------------//
-function Devate({ path, setIsEditing, postInfo, setPostInfo }: { path: string; setIsEditing: any; postInfo: any; setPostInfo: any }) {
+export default function CommonPostEditForm({
+  path,
+  setIsEditing,
+  postInfo,
+  setPostInfo,
+}: {
+  path: string
+  setIsEditing: any
+  postInfo: any
+  setPostInfo: any
+}) {
   const onChange = (e: any) => handleChange({ event: e, someState: postInfo, setSomeState: setPostInfo })
   const onClick = () => handleEdit({ endpoint: `${path}/${postInfo._id}`, data: postInfo, callback: setIsEditing })
 
-  return (
-    <div>
-      <Container component="main" maxWidth="xs">
+  //-------------------------------------------Devate-------------------------------------------//
+  function Devate() {
+    return (
+      <>
         <TextFieldAtom id="title" label="title" name="title" value={postInfo.title} onChange={onChange} />
         <TextFieldAtom id="content" label="content" name="content" value={postInfo.content} onChange={onChange} />
         <TextFieldAtom id="tag" label="tag" name="tag" value={postInfo.tag} onChange={onChange} />
-        <button onClick={onClick}>게시글 수정하기</button>
-        <button
-          onClick={() => {
-            setIsEditing(false)
-          }}
-        >
-          취소
-        </button>
-      </Container>
-    </div>
-  )
-}
-
-//-------------------------------------------Philosopher-------------------------------------------//
-function Philosopher({ path, setIsEditing, postInfo, setPostInfo }: { path: string; setIsEditing: any; postInfo: any; setPostInfo: any }) {
-  const onChange = (e: any) => handleChange({ event: e, someState: postInfo, setSomeState: setPostInfo })
-  const onClick = () => handleEdit({ endpoint: `${path}/${postInfo._id}`, data: postInfo, callback: setIsEditing })
-
-  return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <TextFieldAtom id="title" label="title" name="title" value={postInfo.title} onChange={onChange} />
-        <TextFieldAtom id="content" label="content" name="content" value={postInfo.content} onChange={onChange} />
-        <TextFieldAtom id="tag" label="tag" name="tag" value={postInfo.tag} onChange={onChange} />
-        <button onClick={onClick}>게시글 수정하기</button>
-        <button
-          onClick={() => {
-            setIsEditing(false)
-          }}
-        >
-          취소
-        </button>
-      </Container>
-    </div>
-  )
-}
-
-//-------------------------------------------exchange-------------------------------------------//
-function Exchange({ path, postList }: { path: string; postList: Post[] }) {
-  switch (path) {
-    case "devates":
-      return <Devate postList={postList} />
-    case "philosopher":
-      return <Philosopher postList={postList} />
-    default:
-      return <p>잘못된 경로입니다.</p>
+      </>
+    )
   }
-}
 
-export default function CommonPostEditForm() {}
+  //-------------------------------------------Philosopher-------------------------------------------//
+  function Philosopher() {
+    return (
+      <>
+        <TextFieldAtom id="title" label="title" name="title" value={postInfo.title} onChange={onChange} />
+        <TextFieldAtom id="content" label="content" name="content" value={postInfo.content} onChange={onChange} />
+        <TextFieldAtom id="tag" label="tag" name="tag" value={postInfo.tag} onChange={onChange} />
+      </>
+    )
+  }
+
+  //-------------------------------------------exchange-------------------------------------------//
+  function Exchange() {
+    switch (path) {
+      case "devates":
+        return <Devate />
+      case "philosopher":
+        return <Philosopher />
+      default:
+        return <p>잘못된 경로입니다.</p>
+    }
+  }
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <Exchange />
+        <button onClick={onClick}>게시글 수정하기</button>
+        <button
+          onClick={() => {
+            setIsEditing(false)
+          }}
+        >
+          취소
+        </button>
+      </Container>
+    </div>
+  )
+}
