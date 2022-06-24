9ad5be08a8ebfed69d956c4bea8c292bf7ba172c
hajun <audgkwns44@gmail.com>
2022-06-24 17:15:49 +0900

    Applying prettier to .ts, .tsx

--- front/src/components/templates/LoginTemplate.tsx
+++ front/src/components/templates/LoginTemplate.tsx
@@ -1,24 +1,31 @@
-import { Link } from "react-router-dom"
-import Container from "@mui/material/Container"
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
 
-import LoginForm from "../organisms/LoginForm"
-import Header from "../organisms/Header"
+import LoginForm from "../organisms/LoginForm";
+import Header from "../organisms/Header";
 
 type User = {
-  email: string
-  password: string
-  name: string
-}
-export default function LoginTemplate({ login, userInfo }: { login?: boolean; userInfo?: User }) {
+  email: string;
+  password: string;
+  name: string;
+};
+export default function LoginTemplate({
+  login,
+  userInfo,
+}: {
+  login?: boolean;
+  userInfo?: User;
+}) {
   const initUser: User = {
     email: "",
     password: "",
     name: "",
-  }
+  };
 
   const Footer = () => {
-    return <p>푸터도 오게 될까요?</p>
-  }
+    return <p>푸터도 오게 될까요?</p>;
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Header />
@@ -26,5 +33,5 @@ export default function LoginTemplate({ login, userInfo }: { login?: boolean; us
       {!login && <Link to="/user/register">회원가입</Link>}
       <Footer />
     </Container>
-  )
+  );
 }
