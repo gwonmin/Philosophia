6b4842c7abc499bddd187d462890164cf7c2386a
Hyun <watalu1209@gmail.com>
2022-06-24 10:16:34 +0900

    [Update]: C-P-Cards를 수정했습니다.

--- front/src/components/organisms/CommonPostCards.tsx
+++ front/src/components/organisms/CommonPostCards.tsx
@@ -1,70 +1,62 @@
 import { Link } from "react-router-dom"
 import { Post } from "../templates/CommonPageTemplate"
 
-//-------------------------------------------Devate-------------------------------------------//
-function Devate({ postList }: { postList: Post[] }) {
-  console.log("location: Devate, postList: ", postList)
-  return (
-    <>
-      <p>토론 목록:</p>
-      {postList.map((post: Post) => {
-        return (
-          <div key={post._id} style={{ backgroundColor: "grey" }}>
-            <Link to={post._id}>
-              <p>제목: {post.title}</p>
-              <p>글쓴이: {post.author.name}</p>
-              <p>태그: {post.tag}</p>
-              <p>
-                찬성: {post.yes.length}, 반대: {post.no.length}
-              </p>
-              <p>덧글 수: {post.comment.length}</p>
-            </Link>
-          </div>
-        )
-      })}
-    </>
-  )
-}
-
-//-------------------------------------------Philosopher-------------------------------------------//
-function Philosopher({ postList }: { postList: Post[] }) {
-  console.log("location: Philosopher, postList: ", postList)
-
-  return (
-    <>
-      <p>토론 목록:</p>
-      {postList.map((post: Post) => {
-        return (
-          <div key={post._id} style={{ backgroundColor: "grey" }}>
-            <Link to={post._id}>
-              <p>제목: {post.title}</p>
-              <p>글쓴이: {post.author.name}</p>
-              <p>덧글 수: {post.comment.length}</p>
-            </Link>
-          </div>
-        )
-      })}
-    </>
-  )
-}
-
-//-------------------------------------------exchange-------------------------------------------//
-function Exchange({ path, postList }: { path: string; postList: Post[] }) {
-  switch (path) {
-    case "devates":
-      return <Devate postList={postList} />
-    case ":who":
-      return <Philosopher postList={postList} />
-    default:
-      return <p>잘못된 경로입니다.</p>
-  }
-}
-
 export default function CommonPostCards({ currentPage, postList }: { currentPage: any; postList: Post[] }) {
+  //-------------------------------------------Devate-------------------------------------------//
+  function Devate({ post }: { post: any }) {
+    return (
+      <>
+        <p>제목: {post.title}</p>
+        <p>글쓴이: {post.author.name}</p>
+        <p>태그: {post.tag}</p>
+        <p>
+          찬성: {post.yes.length}, 반대: {post.no.length}
+        </p>
+        <p>덧글 수: {post.comment.length}</p>
+      </>
+    )
+  }
+
+  //-------------------------------------------Philosopher-------------------------------------------//
+  function Philosopher({ post }: { post: any }) {
+    return (
+      <>
+        <p>제목: {post.title}</p>
+        <p>글쓴이: {post.author.name}</p>
+        <p>덧글 수: {post.comment.length}</p>
+      </>
+    )
+  }
+
+  //-------------------------------------------exchange-------------------------------------------//
+  function Exchange({ post }: { post: any }) {
+    console.log("locatiom: Exchange, post: ", post)
+    switch (currentPage.DEFAULT.path) {
+      case "devates":
+        return <Devate post={post} />
+      case ":who":
+        return <Philosopher post={post} />
+      default:
+        return <p>잘못된 경로입니다.</p>
+    }
+  }
   return (
     <div>
       {postList.length == 0 && <p>아직 게시물이 없네요.</p>}
-      {postList != [] && <Exchange path={currentPage.DEFAULT.path} postList={postList} />}
+      {postList != [] && (
+        <div>
+          <p>토론 목록:</p>
+          {postList.map((post: Post) => {
+            return (
+              <div key={post._id} style={{ backgroundColor: "grey" }}>
+                <Link to={post._id}>
+                  <Exchange post={post} />
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
