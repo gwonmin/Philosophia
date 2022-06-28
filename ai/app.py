562628c83c9e3f998fa917d9e38ca2d0dbbcb669
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-06-28 17:19:51 +0900

    [Fix] checkcomment 기능 작업중

--- ai/app.py
+++ ai/app.py
@@ -7,7 +7,8 @@ import flask
 
 from flask import Flask, request
 from tqdm import trange
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, BertForSequenceClassification, BertTokenizer
+from keras_preprocessing.sequence import pad_sequences
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -82,11 +83,49 @@ def generate(
                 
     return generated_list
 
+
+#Get the tokenizer and model
+model_hate = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased", num_labels=2)
+tokenizer_hate = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+
+#load models
+model_hate.load_state_dict(torch.load('./hate.pt', map_location=torch.device('cpu')))
+
+# 입력 데이터 변환
+def convert_input_data(sentences):
+
+    # BERT의 토크나이저로 문장을 토큰으로 분리
+    tokenized_texts = [tokenizer_hate.tokenize(sent) for sent in sentences]
+
+    # 입력 토큰의 최대 시퀀스 길이
+    MAX_LEN = 128
+
+    # 토큰을 숫자 인덱스로 변환
+    input_ids = [tokenizer_hate.convert_tokens_to_ids(x) for x in tokenized_texts]
+    
+    # 문장을 MAX_LEN 길이에 맞게 자르고, 모자란 부분을 패딩 0으로 채움
+    input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+    # 어텐션 마스크 초기화
+    attention_masks = []
+
+    # 어텐션 마스크를 패딩이 아니면 1, 패딩이면 0으로 설정
+    # 패딩 부분은 BERT 모델에서 어텐션을 수행하지 않아 속도 향상
+    for seq in input_ids:
+        seq_mask = [float(i>0) for i in seq]
+        attention_masks.append(seq_mask)
+
+    # 데이터를 파이토치의 텐서로 변환
+    inputs = torch.tensor(input_ids)
+    masks = torch.tensor(attention_masks)
+
+    return inputs, masks
+
+
 @app.route('/inference', methods=['POST'])
 #Function to generate multiple sentences. data should be a dataframe
 def text_generation():
   req_data = request.json
-  print(req_data)
   data = pd.DataFrame(data=[req_data['content']], index=range(0,1), columns=['0'])
   generated = []
   for i in range(len(data)):
@@ -103,6 +142,43 @@ def text_generation():
   result = flask.Response(result)
   result.headers["Access-Control-Allow-Origin"] = "*"
   return result
+
+@app.route('/checkcomment', methods=['POST'])
+# 문장 테스트
+def test_sentences():
+
+    sentences = request.body
+    print(sentences)
+
+    # 평가모드로 변경
+    model_hate.eval()
+
+    # 문장을 입력 데이터로 변환
+    inputs, masks = convert_input_data(sentences)
+
+    # 데이터를 CPU에 넣음
+    device = torch.device("cpu")
+    b_input_ids = inputs.to(device)
+    b_input_mask = masks.to(device)
+            
+    # 그래디언트 계산 안함
+    with torch.no_grad():     
+        # Forward 수행
+        outputs = model_hate(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask)
+
+    # 출력 로짓 구함
+    logits = outputs[0]
+
+    # CPU로 데이터 이동
+    logits = logits.detach().cpu().numpy()
+    result = np.argmax(logits)
+    
+    #CORS 설정
+    result = flask.Response(result)
+    result.headers["Access-Control-Allow-Origin"] = "*"
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,threaded = False)
