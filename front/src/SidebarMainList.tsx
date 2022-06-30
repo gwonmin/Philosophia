34205643f168baef2c474dcc621cc75289402d48
hajun <audgkwns44@gmail.com>
2022-06-30 18:58:11 +0900

    [Update]styling intersave

---
+++ front/src/SidebarMainList.tsx
@@ -0,0 +1,13 @@
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      기타
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="팀 소개" />
+    </ListItemButton>
+  </React.Fragment>
+)
