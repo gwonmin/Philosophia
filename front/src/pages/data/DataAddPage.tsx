a9c4c9a57e5555d4ec9419a3b6920600c32d7b82
hajun <audgkwns44@gmail.com>
2022-06-30 09:31:59 +0900

    [Update]data add form styling

--- front/src/pages/data/DataAddPage.tsx
+++ front/src/pages/data/DataAddPage.tsx
@@ -1,14 +1,26 @@
-import DataPostAddForm from "../../components/organisms/DataPostAddForm";
-import Header from "../../components/organisms/Header";
-import Footer from "../../components/organisms/Footer";
+import DataPostAddForm from "../../components/organisms/DataPostAddForm"
+import Header from "../../components/organisms/Header"
+import Footer from "../../components/organisms/Footer"
+import { Container, Typography } from "@mui/material"
+
+const addFormHeaderStyle = {
+  fontWeight: "bold",
+  borderBottom: "2px black solid",
+  mb: 2,
+}
 
 export default function DataAddPage() {
   //변수 초기화
   return (
     <>
       <Header />
-      <DataPostAddForm />
+      <Container component="main" maxWidth="md">
+        <Typography variant="h5" sx={addFormHeaderStyle}>
+          새 글 작성하기
+        </Typography>
+        <DataPostAddForm />
+      </Container>
       <Footer />
     </>
-  );
+  )
 }
