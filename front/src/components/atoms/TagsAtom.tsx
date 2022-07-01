d636055acb7596a9c964163792736402b151fef0
Hyun <watalu1209@gmail.com>
2022-07-01 17:14:34 +0900

    [Fix]: 태그 오류 해결

--- front/src/components/atoms/TagsAtom.tsx
+++ front/src/components/atoms/TagsAtom.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import Chip from "@mui/material/Chip"
 
-const TagsAtom: React.FC<{ tags?: string[] }> = ({ tags }) => {
+const TagsAtom: React.FC<{ tags?: string[] | string }> = ({ tags }) => {
+  if (!tags) return <p>loading...</p>
+  console.log(tags)
+  if (typeof tags === "string") return <Chip size="small" sx={{ fontSize: 10, mr: 0.5 }} color="primary" label={`# ${tags || "(빈 태그)"}`} />
   return (
     <>
-      {tags?.map((tag) => {
+      {tags.map((tag) => {
         return <Chip key={tag} size="small" sx={{ fontSize: 10, mr: 0.5 }} color="primary" label={`# ${tag || "(빈 태그)"}`} />
       })}
     </>
